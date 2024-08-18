407e82e24429d638244ef7d7929247b6d76bf048
Yuriy Artamonov <yuriy.artamonov@jetbrains.com>
2024-08-18 11:38:22 +0200

    Upgrade build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,55 +1,53 @@
+import org.jetbrains.intellij.platform.gradle.TestFrameworkType
+
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.14.2"
+    id("org.jetbrains.intellij.platform") version "2.0.1"
 }
 
 group = "org.strangeway.tsr"
-version = "1.1.0"
+version = "1.2.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
+}
+
+dependencies {
+    intellijPlatform {
+        create("IC", "2024.2")
+        testFramework(TestFrameworkType.Platform)
+        instrumentationTools()
+    }
+    testImplementation("junit:junit:4.13.2")
+}
+
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "242"
+            untilBuild = "251.*"
+        }
+
+        changeNotes = """
+          <ul>
+            <li>Support IntelliJ IDEA 2024.2</li>
+          </ul>
+        """.trimIndent()
+    }
 }
 
 java.sourceSets["main"].java {
     srcDir("src/main/gen")
 }
 
-tasks.withType<JavaCompile> {
-    options.encoding = "UTF-8"
-}
-
-intellij {
-    version.set("2023.1.3")
-    type.set("IC") // Target IDE Platform
-
-    plugins.set(listOf())
-}
-
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
+        options.encoding = "UTF-8"
         sourceCompatibility = "17"
         targetCompatibility = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("231")
-        untilBuild.set("241.*")
-
-        changeNotes.set("""
-          <ul>
-            <li>Support IntelliJ IDEA 2023.1</li>
-          </ul>
-        """.trimIndent())
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
     }
 }
